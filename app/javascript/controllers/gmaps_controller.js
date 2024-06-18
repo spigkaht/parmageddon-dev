1b8a06e6a9da23f411c6e6e8ac13695cf510a977
Benjamin Jackson <benj@betterrentals.com.au>
2024-06-19 08:05:58 +1000

    progress on google maps api

--- app/javascript/controllers/gmaps_controller.js
+++ app/javascript/controllers/gmaps_controller.js
@@ -5,34 +5,27 @@ export default class extends Controller {
   static targets = [ "mapDiv" ];
   static values = {
     apiKey: String,
-    locationLat: Number,
-    locationLng: Number,
-    markers: Array,
+    // locationLat: Number,
+    // locationLng: Number,
+    // markers: Array,
     venues: Array
   }
 
   connect() {
-    const city = {
-      lat: this.locationLatValue,
-      lng: this.locationLngValue
-    };
-
     const loader = new Loader({
       apiKey: this.apiKeyValue,
       version: "weekly",
       libraries: ["maps"]
     });
 
-    const mapOptions = {
-      center: { lat: city.lat, lng: city.lng },
-      zoom: 10,
-    };
-
     loader
     .importLibrary('maps')
-    .then(({ Map, Marker }) => {
+    .then(({ Map, Marker, Geocoder }) => {
       const bounds = new google.maps.LatLngBounds();
+      const geocoder = new google.maps.Geocoder();
+      const latlng = new google.maps.LatLng(-37.8136, 144.9631)
       const markerURL = "https://res.cloudinary.com/dp0apr6y4/image/upload/v1718612885/chicken-marker_rivnug.svg"
+
       const icon = {
         url: markerURL,
         scaledSize: new google.maps.Size(50, 50), // Scale the SVG
@@ -40,28 +33,40 @@ export default class extends Controller {
         labelOrigin: new google.maps.Point(25, -15)
       };
 
-      const map = new Map(this.mapDivTarget, mapOptions);
-      console.log("position INCORRECT: ", this.markersValue);
+      const mapOptions = {
+        center: latlng,
+        zoom: 10,
+      };
 
-      this.markersValue.forEach(marker => {
-        const markerOptions = {
-          position: {
-            lat: parseFloat(marker.lat),
-            lng: parseFloat(marker.lng)
-          },
-          label: {
-            text: marker.name,
-            className: "gmaps-marker",
-            fontSize: "16px",
-            fontWeight: "700",
-            fontFamily: "Roboto"
-          },
-          icon: icon,
-          map: map
-        };
-        console.log("position INCORRECT: ", markerOptions.position);
-        const gmapsMarker = new google.maps.Marker(markerOptions);
-        bounds.extend(gmapsMarker.getPosition());
+      const map = new Map(this.mapDivTarget, mapOptions);
+
+      this.venuesValue.forEach(venue => {
+        const address = venue.street.concat(", ", venue.city, ", ", venue.state, ", ", venue.zip)
+        geocoder.geocode({"address": address}, function(results, status) {
+          if (status == "OK") {
+            const center = results[0].geometry.location;
+            console.log("address: ", address);
+            console.log("results: ", results[0]);
+            const markerOptions = {
+              label: {
+                text: venue.name,
+                className: "gmaps-marker",
+                fontSize: "16px",
+                fontWeight: "700",
+                fontFamily: "Roboto"
+              },
+              position: results[0].geometry.location,
+              icon: icon,
+              map: map
+            };
+
+            const marker = new google.maps.Marker(markerOptions);
+
+            bounds.extend(marker.getPosition());
+          } else {
+            alert("Geocode was not successful for the following reason: " + status);
+          }
+        });
       })
       map.fitBounds(bounds);
     })
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -16,11 +16,4 @@ class Venue < ApplicationRecord
 
   geocoded_by :address
   after_save :geocode
-
-  after_validation :log_address_change, if: :street_changed?
-
-  def log_address_change
-    Rails.logger.debug "Coordinates changed from #{longitude_was} to #{longitude}"
-    Rails.logger.debug "Street address changed from #{street_was} to #{street}"
-  end
 end
