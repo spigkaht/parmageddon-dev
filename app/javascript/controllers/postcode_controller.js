a87aff726c61fa367b0161536773e95733128078
Benjamin Jackson <benj@betterrentals.com.au>
2024-06-24 15:17:39 +1000

    progress on google maps api, started creating places instances

---
+++ app/javascript/controllers/postcode_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["input"]
+
+  connect() {
+    console.log("hello from the postcode controller!")
+  }
+
+  submit(event) {
+    event.preventDefault();
+    const inputValue = this.inputTarget.value;
+    const form = this.element;
+    form.action = `/map?p=${encodeURIComponent(inputValue)}`;
+    form.submit();
+  }
+}
