b1b0c5505c0b94b2e123d1839bf082dd47d2036a
Benjamin Jackson <benj@betterrentals.com.au>
2024-06-28 11:52:34 +1000

    ..

--- app/services/create_venues_by_places.rb
+++ app/services/create_venues_by_places.rb
@@ -2,15 +2,17 @@ class CreateVenuesByPlaces
   def self.create_venues(venues_data, postcode)
     venues_data.map do |venue|
       Venue.find_by(name: venue["name"]) ||
-      (venue = Venue.new(name: venue["name"],
-                     street: venue["vicinity"].split(',')[0],
-                     city: venue["vicinity"].split(',')[1],
-                     state: "VIC",
-                     postcode: postcode,
-                     latitude: venue["geometry"]["location"]["lat"],
-                     longitude: venue["geometry"]["location"]["lng"],
-                     rating_average: 0.0,
-                     price_average: 0.0))
+        Venue.new(
+          name: venue["name"],
+          street: venue["vicinity"].split(',')[0],
+          city: venue["vicinity"].split(',')[1],
+          state: "VIC",
+          postcode: postcode,
+          latitude: venue["geometry"]["location"]["lat"],
+          longitude: venue["geometry"]["location"]["lng"],
+          rating_average: 0.0,
+          price_average: 0.0
+        )
     end
   end
 end
