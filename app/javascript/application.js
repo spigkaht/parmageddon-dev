154847796230407e8eae8c740f39e9df7ee0d687
Benjamin Jackson <benj@betterrentals.com.au>
2024-07-03 10:49:37 +1000

    google maps marker & label adjustments

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Application } from "@hotwired/stimulus"
+import GmapsController from "./controllers/gmaps_controller"
+import CarouselController from "./controllers/carousel_controller"
+
+const application = Application.start()
+application.register("gmaps", GmapsController)
+application.register("carousel", CarouselController)
